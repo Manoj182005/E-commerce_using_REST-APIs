dec05f251f0a17a1f0cac7b8c496e688238f616c
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-12-24 22:28:01 +0530

    REST apis

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-import  express from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import productRouter from './src/features/product/product.routes.js';
 
 const server = express();
+
+server.use(bodyParser.json());
+
+server.use("/api/products", productRouter);
 
 server.get("/",(req, res)=>{
     res.send("Welcome to E-commerce API")
---
+++ src/features/product/product.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import ProductController from './product.controller.js';
+
+const productRouter = express.Router();
+const productController = new ProductController();
+
+productRouter.get('/', productController.getAllProducts);
+productRouter.post('/', productController.addProduct);
+
+
+export default productRouter;
