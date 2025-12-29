48aab3e4c7f34b52126d19fe981a6271a7731b5b
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-12-29 23:05:08 +0530

    about cors

--- src/features/cartItems/cartItems.controller.js
+++ src/features/cartItems/cartItems.controller.js
@@ -7,4 +7,25 @@ export class CartItemsController {
         CartItemModel.add(productID, userID, quantity);
         res.status(201).send("Cart is updated");
     }
+
+    get(req, res){
+        const userID = req.userID;
+        const items = CartItemModel.get(userID);
+        return res.status(200).send(items);
+    }
+
+    delete(req, res) {
+        const userID = req.userID;
+        const cartItemID = req.params.id;
+        const error = CartItemModel.delete(
+            cartItemID,
+            userID
+        );
+        if (error) {
+            return res.status(404).send(error);
+        }
+        return res
+        .status(200)
+        .send('Cart item is removed');
+    }
 }
