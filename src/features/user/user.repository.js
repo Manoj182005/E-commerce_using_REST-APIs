db940f21e74b02423e9a78361a17da5cc2d00f6c
Manoj Patel BM <manojpatelbm182005@gmail.com>
2026-01-21 18:15:53 +0530

    repository

--- src/features/user/user.repository.js
+++ src/features/user/user.repository.js
@@ -3,12 +3,16 @@ import { ApplicationError } from "../../error-handler/applicationError.js";
 
 class UserRepository{
 
+        constructor(){
+          this.collection = "users";
+      }
+
      async signUp(newUser) {
         try{
           // 1. Get the database
         const db = getDB();
         // 2. Get the collection
-        const collection = db.collection("users");
+        const collection = db.collection(this.collection);
         
         // 3. Insert the document.
         await collection.insertOne(newUser);
@@ -24,10 +28,25 @@ class UserRepository{
           // 1. Get the database
         const db = getDB();
         // 2. Get the collection
-        const collection = db.collection("users");
+        const collection = db.collection(this.collection);
         
         // 3. Find the document.
         return await collection.findOne({email, password});
+        } catch(err){
+          console.log(err);
+          throw new ApplicationError("Something went wrong with database", 500);
+        }
+      }
+
+      async findByEmail(email) {
+        try{
+          // 1. Get the database
+        const db = getDB();
+        // 2. Get the collection
+        const collection = db.collection("users");
+        
+        // 3. Find the document.
+        return await collection.findOne({email});
         } catch(err){
           console.log(err);
           throw new ApplicationError("Something went wrong with database", 500);
